44cb644b8633c0e076741ec5332135fb4bc3ce9a
fulya_ture <fulyature@gmail.com>
2023-07-13 00:22:31 +0300

    props

--- src/components/ContainerCard.jsx
+++ src/components/ContainerCard.jsx
@@ -1,5 +1,12 @@
-const ContainerCard = () => {
-  return <div>ContainerCard</div>;
+const ContainerCard = ({ type, src }) => {
+  return (
+    <div className="img-wrapper">
+      <div className="img-container">
+        <img className="image" src={src} alt="img" />
+      </div>
+      <div className="type">{type}</div>
+    </div>
+  );
 };
 
 export default ContainerCard;
